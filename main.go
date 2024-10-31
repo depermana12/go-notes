931be7bc2aac61a257bccc4c0252c783e7335b5e
depermana12 <deddiapermana97@gmail.com>
2024-11-01 01:24:38 +0700

    add route post handler

--- main.go
+++ main.go
@@ -1,20 +1,86 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/gorm"
+
+	"github.com/depermana12/go-notes/config"
+	"github.com/depermana12/go-notes/models"
 )
 
-func handleFunc(r http.ResponseWriter, w *http.Request) {
-	r.Write([]byte("hello world"))
+var DB *gorm.DB
+
+func init() {
+	var err error
+	DB, err = config.ConnectToDB()
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+
+	err = DB.AutoMigrate(&models.User{}, models.Note{})
+	if err != nil {
+		log.Fatal("failed to migrate schema to database", err)
+	}
+
 }
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", handleFunc)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Post("/user", createUser)
+		r.Post("/notes", createNote)
+	})
 
+	log.Println("server listening to port 3000")
 	http.ListenAndServe(":3000", r)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	err = DB.Create(&user).Error
+
+	if err != nil {
+		http.Error(w, "failed to create user", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+
+}
+
+func createNote(w http.ResponseWriter, r *http.Request) {
+	var note models.Note
+
+	err := json.NewDecoder(r.Body).Decode(&note)
+
+	if err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	err = DB.Create(&note).Error
+
+	if err != nil {
+		http.Error(w, "failed to create note", http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(note)
 }
---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" gorm:"uniqueIndex"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	Notes    []Note `json:"notes" gorm:"foreignKey:AuthorId"`
+}
