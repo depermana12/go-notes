802b1a3a6e0c61bea46c627f4ffa48e8745b0a70
depermana12 <deddiapermana97@gmail.com>
2024-11-03 15:54:40 +0700

    add yup login validation

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,19 +1,37 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { InferType } from "yup";
+
+const loginSchema = yup.object({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+type FormData = InferType<typeof loginSchema>;
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<{
-    email: string;
-    password: string;
-  }>();
-  const onSubmit = (data: { email: string; password: string }): void => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = (data: FormData) => {
     console.log(data);
+    console.log("hello");
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email"></label>
+      <label htmlFor="email">email: </label>
       <input type="email" id="email" {...register("email")} />
+      <p>{errors.email?.message}</p>
+      <label htmlFor="password">password: </label>
       <input type="password" id="password" {...register("password")} />
+      <p>{errors.password?.message}</p>
       <button type="submit">login</button>
     </form>
   );
